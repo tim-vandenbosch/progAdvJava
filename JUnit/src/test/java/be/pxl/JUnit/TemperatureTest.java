b84ee6a57c458cff490a52c5db3edac977e99c01
Tim Van Den Bosch <tim_vandenbosch@msn.com>
2017-02-21 17:47:44 +0100

    Add Temperature Tester

---
+++ JUnit/src/test/java/be/pxl/JUnit/TemperatureTest.java
@@ -0,0 +1,42 @@
+package be.pxl.JUnit;
+import org.junit.*;
+import org.junit.runners.Parameterized;
+
+import static org.junit.Assert.*;
+
+
+/**
+ * Created by tim_v on 21/02/2017.
+ */
+public class TemperatureTest
+{
+    @Test
+    public void testConstructor()
+    {
+        new Temperature(10);
+    }
+
+    @Test
+    public void testValue()
+    {
+        // test getters and setter
+        Temperature temp;
+        temp = new Temperature(10);
+        temp.setTemp(10);
+        long gottenTemp = temp.getTemp();
+
+        assertEquals(10,gottenTemp);
+    }
+
+    @Test
+    public void testEquals()
+    {
+        fail();
+    }
+
+    @Test
+    public void testHashCode()
+    {
+        fail();
+    }
+}
